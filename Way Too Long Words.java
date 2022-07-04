68909bc35fb3db68225e100a8d3aa8212fc27c68
Mina Victor <107629210+MinaVictor1@users.noreply.github.com>
2022-07-05 00:30:30 +0200

    Update Way Too Long Words.java

--- Way Too Long Words.java
+++ Way Too Long Words.java
@@ -1,21 +1,20 @@
 import java.util.Scanner;
-import javax.lang.model.util.ElementScanner14;
-class HelloWorld { 
-
-   public static void main (String[] args) 
-   {
+ 
+public class Mavenproject2 {
+ 
+    public static void main(String[] args) {
       Scanner in =new Scanner(System.in);
       int x=in.nextInt();
       for(int i=0;i<=x;i++){
          String str=in.nextLine();
-         if(str.length()>=5){
+         if(str.length()>10){
             System.out.print(str.charAt(0));
             System.out.print(str.length()-2);
            System.out.println(str.charAt(str.length()-1));
          }else{
             System.out.println(str);
          }
-
+ 
       }
-   }   
-}   
+    }
+}
