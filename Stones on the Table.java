1d168b69015fa5e01c59e14979b9517fec41b99f
Mina Victor <107629210+MinaVictor1@users.noreply.github.com>
2022-07-06 21:03:35 +0200

    Add files via upload

---
+++ Stones on the Table.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import javax.lang.model.util.ElementScanner14;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import javax.xml.transform.Source;
+
+import java.util.Arrays;
+
+import java.util.Scanner;
+class HelloWorld { 
+public static void main (String[] args) 
+  {
+    Scanner in=new Scanner(System.in);
+    int x=in.nextInt();
+    char []arr=new char[x];
+    String str=in.next();
+    int count=0;
+    for(int i=0;i<x;i++){
+      arr[i]=str.charAt(i);
+    }
+    for(int j=0;j<=x-2;j++){
+      for(int y=j+1;y<=j+1;y++){
+        if(arr[j]==arr[y]){
+          count++;
+          break;
+        }
+      }
+    }
+    System.out.println(count);
+  }
+}
