3acb242012cbc0a5803cd47f61287254725a1a53
Mina Victor <107629210+MinaVictor1@users.noreply.github.com>
2022-07-08 00:41:33 +0200

    Add files via upload

---
+++ wrong Subtraction.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import javax.lang.model.util.ElementScanner14;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import javax.xml.transform.Source;
+
+import java.util.Arrays;
+
+import java.util.Scanner;
+class HelloWorld { 
+public static void main (String[] args) 
+  {
+    Scanner in=new Scanner(System.in);
+    int x =in.nextInt();
+    int y=in.nextInt();
+    for(int i =1;i<=y;i++){//510
+      if(x%10==0){
+        x/=10;
+      }else{
+        x-=1;
+      }
+    }
+    System.out.println(x);
+  }
+}
