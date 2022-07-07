bc66d3cf22e0fd2fa00bdfde9d84431d408fbcef
Mina Victor <107629210+MinaVictor1@users.noreply.github.com>
2022-07-07 02:30:55 +0200

    Add files via upload

---
+++ Soldier and Bananas.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import javax.lang.model.util.ElementScanner14;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import javax.xml.transform.Source;
+
+import java.util.Arrays;
+
+import java.util.Scanner;
+class HelloWorld { 
+public static void main (String[] args) 
+  {
+    Scanner in =new Scanner(System.in);
+    int banana_price=in.nextInt();
+    int money=in.nextInt();
+    int n_banana=in.nextInt();
+    int x=0;
+    for(int i=1;i<=n_banana;i++){
+      x+=i;
+    }
+    int total_price=banana_price*x;
+    if((total_price-money)<0){
+      System.out.println("0");
+    }else{
+    System.out.println(total_price-money);
+  }
+  }
+}
