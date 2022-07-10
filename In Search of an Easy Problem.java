d5145f0f24df02d5506e464504b4d350908cc593
Mina Victor <107629210+MinaVictor1@users.noreply.github.com>
2022-07-11 00:45:41 +0200

    Add files via upload

---
+++ In Search of an Easy Problem.java
@@ -0,0 +1,34 @@
+import java.util.Arrays;
+import java.util.Scanner;
+import javax.lang.model.util.ElementScanner14;
+import javax.print.DocFlavor.STRING;
+import javax.swing.plaf.synth.SynthOptionPaneUI;
+import javax.xml.namespace.QName;
+import javax.xml.transform.Source;
+
+import java.util.Arrays;
+
+import java.util.Scanner;
+class HelloWorld {
+public static void main (String[] args) 
+  {
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int v=0;
+    for(int i=1;i<=n;i++){
+        int z=sc.nextInt();
+        if(z==1){
+            v++;
+        }
+    }
+    
+    if(v>0){
+        System.out.println("HARD");
+    }else{
+        System.out.println("EASY");
+    }
+  }
+}
+
+
+
