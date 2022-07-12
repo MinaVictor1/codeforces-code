0fbcc1f020c25de801936dd10cd418fd6e09ee09
Mina Victor <107629210+MinaVictor1@users.noreply.github.com>
2022-07-12 17:19:40 +0200

    Update Calculating Function.java

--- Calculating Function.java
+++ Calculating Function.java
@@ -1,30 +1,17 @@
-import java.util.Arrays;
-import java.util.Scanner;
-import javax.lang.model.util.ElementScanner14;
-import javax.print.DocFlavor.STRING;
-import javax.swing.plaf.synth.SynthOptionPaneUI;
-import javax.xml.namespace.QName;
-import javax.xml.transform.Source;
-
-import java.util.Arrays;
-/*
-  -1+2-3+4-5+6-7+8-9+10-11+12-13+14-15+16
-*/
-
 import java.util.Scanner;
 class HelloWorld {
 public static void main (String[] args) 
   {
     Scanner in=new Scanner(System.in);//-2
-    int x=in.nextInt();
-    int p=-1;
-    for(int i=2;i<=x;i++){
-      if(i%2==0){
-        p+=i;
+    Scanner in=new Scanner(System.in);
+    long x=in.nextLong();
+      if(x%2==0){
+        System.out.println(x/2);
       }else{
-        p-=i;
+        x-=1;
+        x/=2;
+        x+=1;
+        System.out.println(-1*x);
       }
-    }
-    System.out.println(p);
   }
 }
